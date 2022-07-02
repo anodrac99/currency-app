25238902a31b43dd8ac2840688238733a32d6c91
anodrac99 <juancho-cm2000@hotmail.com>
2022-07-02 13:13:26 -0500

    end

--- src/components/MainInfo.js
+++ src/components/MainInfo.js
@@ -4,37 +4,57 @@ import axios from 'axios';
 
 const MainInfo = () => {
 
-    const [ historical, setHistorical ] = useState({})
+    const [ historical, setHistorical ] = useState([])
 
-    const [ lastest, setLastest ] = useState({})
+    const [ lastest, setLastest ] = useState([])
 
-    const x = async () => {
-       await axios.get('http://localhost:3000/api/v1/lastest/')
-            .then(res => console.log(res.data))
-            .catch(error => console.log(error))
-    }
     useEffect(() => {
-        /* axios.get('https://currencyappjecm.herokuapp.com/api/v1/lastest')
-            .then(res => console.log(res.data))
-            .catch(error => console.log(error)) */
-            x()
+        axios.get('https://currencyappjecm.herokuapp.com/api/v1/lastest')
+            .then(res => setLastest(res.data.info))
+            .catch(error => console.log(error))   
+    }, [])
+
+    useEffect(() => {
+        axios.get('https://currencyappjecm.herokuapp.com/api/v1/historical')
+            .then(res => setHistorical(res.data.info))
+            .catch(error => console.log(error))   
     }, [])
 
     return (
-        <div className='mainContainer'>
-            <div>
-                <h2>Historical</h2>
-                {
-
-                }
+        <div>
+            <p className='infoText'>Change base currency USD</p>
+            <div className='mainContainer'>
+                <div className='historicalContainer'>
+                    <h2>Historical</h2>
+                    {
+                        historical.map(data => (
+                            <ul className='listContainer' key={data.name}>
+                                <li className='nameContainer'>
+                                {data.name}
+                                </li>
+                                <li className='valueContainer'>
+                                {data.value}
+                                </li>
+                            </ul>
+                        ))
+                    }
+                </div>
+                <div className='lastestContainer'>
+                    <h2>Lastest</h2>
+                    {
+                        lastest.map(data => (
+                            <ul className='listContainer' key={data.name}>
+                                <li className='nameContainer'>
+                                    {data.name}
+                                </li>
+                                <li className='valueContainer'>
+                                    {data.value}
+                                </li>
+                            </ul>
+                        ))
+                    }
+                </div>
             </div>
-            <div>
-                <h2>Lastest</h2>
-                {
-
-                }
-            </div>
-            
         </div>
     );
 };
